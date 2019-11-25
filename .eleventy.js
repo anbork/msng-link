240993735efb64ea143e5ebd4c2af6baafe08f63
tonorzenko <tonorzenko@gmail.com>
2019-11-25 13:05:54 +0200

    Testing 404 page with linking messengers

--- .eleventy.js
+++ .eleventy.js
@@ -4,10 +4,8 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.setBrowserSyncConfig({
     callbacks: {
       ready: function(err, bs) {
-        const content_404 = fs.readFileSync('docs/404.html');
-
         bs.addMiddleware("*", (req, res) => {
-          // Provides the 404 content without redirect.
+          const content_404 = fs.readFileSync('docs/404.html');
           res.write(content_404);
           res.end();
         });
@@ -15,11 +13,9 @@ module.exports = function(eleventyConfig) {
     }
   });
 
-  eleventyConfig.addPassthroughCopy("css");
-  
   return {
     dir: {
-      input: "src",
+      input: "views",
       output: "docs"
     }
   };
